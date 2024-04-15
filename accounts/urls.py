bfbfe87d321489bf5d19f86a583b2a2adb63cca7
renyagi <r.yagi@4nonome.com>
2024-04-15 14:39:50 +0900

    プロフィール画面の作成

--- accounts/urls.py
+++ accounts/urls.py
@@ -9,9 +9,13 @@ app_name = "accounts"
 
 urlpatterns = [
     path("signup/", views.SignupView.as_view(), name="signup"),
-    path('login/', LoginView.as_view(template_name="accounts/login.html",form_class=AuthenticationForm ), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
-    # path('<str:username>/', views.UserProfileView.as_view(template_name="{username}.html"), name='user_profile'),
+    path(
+        "login/", LoginView.as_view(template_name="accounts/login.html", form_class=AuthenticationForm), name="login"
+    ),
+    path("logout/", LogoutView.as_view(), name="logout"),
+    path(
+        "<str:username>/", views.UserProfileView.as_view(), name="user_profile"
+    ),
     # path('<str:username>/follow/', views.FollowView.as_view(), name='follow'),
     # path('<str:username>/unfollow/', views.UnFollowView, name='unfollow'),
     # path('<str:username>/following_list/', views.FollowingListView.as_view(), name='following_list'),
--- accounts/views.py
+++ accounts/views.py
@@ -1,14 +1,16 @@
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, get_user_model
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DetailView
+from django.conf import settings
 
 from .forms import SignupForm
 
+User = get_user_model()
 
 class SignupView(CreateView):
     form_class = SignupForm
     template_name = "accounts/signup.html"
-    success_url = reverse_lazy("tweets:home")
+    success_url = reverse_lazy(settings.LOGIN_REDIRECT_URL)
 
     def form_valid(self, form):
         response = super().form_valid(form)
@@ -17,3 +19,12 @@ class SignupView(CreateView):
         user = authenticate(self.request, username=username, password=password)
         login(self.request, user)
         return response
+
+
+class UserProfileView(DetailView):
+    model = User
+    template_name = "accounts/{username}.html"
+
+    def get_object(self, queryset=None):
+        username=self.kwargs.get("username")
+        return self.request.user
