c37236d004e54027d7e1481a306f84b8f55d8492
renyagi <r.yagi@4nonome.com>
2024-04-15 11:26:12 +0900

    各templateの作成及びurlsの修正

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,7 @@
 # from django.contrib.auth import views as auth_views
 from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.forms import AuthenticationForm
 
 from . import views
 
@@ -7,9 +9,9 @@ app_name = "accounts"
 
 urlpatterns = [
     path("signup/", views.SignupView.as_view(), name="signup"),
-    # path('login/', auth_views.LoginView.as_view(), name='login'),
-    # path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    # path('<str:username>/', views.UserProfileView.as_view(), name='user_profile'),
+    path('login/', LoginView.as_view(template_name="accounts/login.html",form_class=AuthenticationForm ), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    # path('<str:username>/', views.UserProfileView.as_view(template_name="{username}.html"), name='user_profile'),
     # path('<str:username>/follow/', views.FollowView.as_view(), name='follow'),
     # path('<str:username>/unfollow/', views.UnFollowView, name='unfollow'),
     # path('<str:username>/following_list/', views.FollowingListView.as_view(), name='following_list'),
