25a370450c77169c2d0945a435612a8635eda5b0
renyagi <r.yagi@4nonome.com>
2024-04-19 17:29:59 +0900

    ログイン・ログアウト機能、プロフィール画面の作成

--- accounts/views.py
+++ accounts/views.py
@@ -1,14 +1,19 @@
-from django.contrib.auth import authenticate, login
+from django.conf import settings
+from django.contrib.auth import authenticate, get_user_model, login
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.views import LoginView
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DetailView
 
 from .forms import SignupForm
+
+User = get_user_model()
 
 
 class SignupView(CreateView):
     form_class = SignupForm
     template_name = "accounts/signup.html"
-    success_url = reverse_lazy("tweets:home")
+    success_url = reverse_lazy(settings.LOGIN_REDIRECT_URL)
 
     def form_valid(self, form):
         response = super().form_valid(form)
@@ -17,3 +22,20 @@ class SignupView(CreateView):
         user = authenticate(self.request, username=username, password=password)
         login(self.request, user)
         return response
+
+
+class UserLoginView(LoginView):
+    form_class = AuthenticationForm
+    template_name = "accounts/login.html"
+
+    def get_success_url(self):
+        username = self.request.user.username
+        return reverse_lazy(settings.LOGIN_REDIRECT_URL, kwargs={"username": username})
+
+
+class UserProfileView(DetailView):
+    model = User
+    template_name = "accounts/{username}.html"
+
+    def get_object(self, queryset=None):
+        return self.get_queryset().get(username=self.kwargs["username"])
